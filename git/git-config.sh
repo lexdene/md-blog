0b82088fddf67634ea2d4625b86092993bd95da4
Elephant Liu <lexdene@sohu.com>
2013-04-12 15:29:40 +0800

    add git config

---
+++ git/git-config.sh
@@ -0,0 +1,13 @@
+git config --global user.name "Elephant Liu"
+git config --global user.email "lexdene at sohu dot com"
+
+git config --global color.branch "auto"
+git config --global color.ui "auto"
+git config --global color.status "auto"
+
+git config --global alias.st "status"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+git config --global alias.ci "commit"
+git config --global alias.co "checkout"
+
+git config --global core.editor "emacs"
