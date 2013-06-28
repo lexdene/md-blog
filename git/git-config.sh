51344cd048e4cc8ac49e0ef88c4217904c6696bf
Elephant Liu <lexdene@sohu.com>
2013-06-28 11:31:28 +0800

    update git config

--- git/git-config.sh
+++ git/git-config.sh
@@ -9,7 +9,7 @@ git config --global alias.st "status"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 git config --global alias.ci "commit"
 git config --global alias.co "checkout"
-git config --global alias.ps "push"
+git config --global alias.ps "push origin HEAD"
 git config --global alias.pl "pull"
 git config --global alias.ap "add -p"
 
