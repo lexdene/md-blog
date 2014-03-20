478924539ca39b3a5d43df367a195fcd0481c9a2
Elephant Liu <lexdene@sohu.com>
2014-03-20 13:37:39 +0800

    update git-config

--- git/git-config.sh
+++ git/git-config.sh
@@ -8,13 +8,18 @@ git config --global color.status "auto"
 git config --global alias.st "status"
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 git config --global alias.ci "commit"
+git config --global alias.cia "commit --amend"
+# 修改commit内容但不修改commit信息
+git config --global alias.ciah "commit --amend -C HEAD"
 git config --global alias.co "checkout"
 git config --global alias.ps "push"
 git config --global alias.pl "pull"
+git config --global alias.fa "fetch --all"
 git config --global alias.ap "add -p"
 git config --global alias.dtr "diff-tree --no-commit-id --name-only -r"
 git config --global alias.dst "diff --name-status"
 git config --global alias.ffm "merge --ff-only"
+git config --global alias.nem "merge --no-edit"
 
 git config --global core.editor "emacs"
 
